71e4d3522a182c793732d40f74f60f54fbf0921b
YANOVSK1Y <andreyianovskyi78@gmail.com>
2021-07-19 19:13:03 +0300

    Try to dix migrations, still don't know how, but have some progress

--- src/pokewiki/pokemons/models.py
+++ src/pokewiki/pokemons/models.py
@@ -6,7 +6,7 @@ class Pokemons(models.Model):
     weight = models.DecimalField(max_digits=20, decimal_places=2)
     types = models.CharField(max_length=30)
     abilities = models.CharField(max_length=100)
-    picture = models.FileField(upload_to='../templates/picture')
-    
+    # picture = models.FileField(upload_to='../templates/picture')
+
     def __str__(self):
         return self.name
